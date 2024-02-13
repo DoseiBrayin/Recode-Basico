c17a5e6e4d88c42a2317d59a69e0c91c5abe3d82
DoseiBrayin <ardilabrayan68@gmail.com>
2024-02-13 16:51:15 -0500

    (add) class 13

---
+++ clases/clase 13/clase13.js
@@ -0,0 +1,45 @@
+// Var -> Globales
+// Let -> Del momento o de la seccion de codigo
+// Const -> Constante
+
+// TIPOS DE DATOS
+var nombre = "Juan";
+var edad = 30;
+var monto = 1000.89;
+var numero = BigInt(10000000000000000);
+var activo = true;
+var hijos = null;
+var hijos2 = undefined;
+var fecha = new Date();
+var objeto = {
+    nombre: "Juan",
+    edad: 30
+};
+var colores = ["Rojo", "Verde", "Azul"];
+
+// OPERADORES
+var suma = 5 + 5;
+var resta = 5 - 5;
+var multiplicacion = 5 * 5;
+var division = 5 / 5;
+var modulo = 5 % 5;
+
+// OPERADORED LOGICOS
+var and = true && true;
+var or = true || false;
+
+// OPERADORES DE COMPARACION
+var igual = 5 == "5";
+var igualTipo = 5 === "5";
+var diferente = 5 != "5"; 
+var diferenteTipo = 5 !== "5";
+var mayor = 5 > 5;
+var menor = 5 < 5;
+var mayorIgual = 5 >= 5;
+var menorIgual = 5 <= 5;
+
+
+
+
+
+
