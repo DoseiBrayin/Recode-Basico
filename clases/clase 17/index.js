0a31f1c89ab59f7f5e944f592c195b1bc75e8d6a
DoseiBrayin <ardilabrayan68@gmail.com>
2024-04-16 16:46:42 -0500

    ejercicio

--- clases/clase 17/index.js
+++ clases/clase 17/index.js
@@ -20,8 +20,21 @@ let mascota = {
 }
 */
 // Compare this snippet from clases/clase%15/index.js:
+/*
+Entidades:
+Comida: Cada comida tiene un nombre, un tipo (por ejemplo, hamburguesa, pizza, tacos), un precio y un estado (disponible o no disponible).
+Cliente: Cada cliente tiene un nombre, un apellido y un número de contacto.
+Restaurante de Comidas Rápidas: El restaurante tiene dos listas, una para las comidas vendidas y otra para las comidas no vendidas.
+Relaciones:
+Compra: Un cliente puede comprar una comida.
+Disponibilidad: Una comida puede estar disponible o no disponible en el restaurante de comidas rápidas.
+Funciones:
+FactoryComida: Esta función crea una nueva comida con los atributos proporcionados.
+FactoryCliente: Esta función crea un nuevo cliente con los atributos proporcionados.
+RestauranteComidasRapidas: Esta función clasifica las comidas en dos listas, vendidas y no vendidas, en el restaurante de comidas rápidas.
+*/
 
-function FactoryMascota(nombre, raza, numeroContacto, dueño) {
+function FactoryMascota(nombre, raza, numeroContacto,IsAtendidos ,dueño) {
     return {
         nombre,
         raza,
@@ -53,13 +66,11 @@ let mascota1 = FactoryMascota("Firulais",
 function Veterinaria(mascota) {
     let listaAtendidos = []
     let listaNoAtendidos = []
-
     if (mascota.IsAtendidos){
         listaAtendidos.push(mascota)
     } else{
         listaNoAtendidos.push(mascota)
     }
-
     return {
         listaAtendidos,
         listaNoAtendidos
@@ -67,7 +78,7 @@ function Veterinaria(mascota) {
 }
 
 console.log(Veterinaria(mascota).listaAtendidos)
-
+console.log(Veterinaria(mascota1).listaAtendidos)
 // Path: clases/clase%2017/index.js
 
 
