76b0652a5e0c71d149ab0cd7028e5ab07d16f09f
DoseiBrayin <ardilabrayan68@gmail.com>
2024-04-16 16:11:09 -0500

    Clase 17

---
+++ clases/clase 17/index.js
@@ -0,0 +1,63 @@
+/*SUSANA TIENE UNA VETERINARIA, QUIERE TENER DOS LISTAS DE ATENDIDOS Y NO ANTETIDOS
+TODAS LAS MASCOTAS TIENEN UN NOMBRE, UNA RAZA, UN NUMERO DE CONTACTO Y UN DUEÑO
+
+let listaAtendidos = []
+let listaNoAtendidos = []
+
+let dueño = {
+    nombre:" ",
+    apellido:" "
+}
+DEBE ENCAPSULARSE
+let mascota = {
+    nombre:"Rocky Ronaldo",
+    raza: "criollo",
+    numeroContacto: "300000123",
+    dueño: dueño
+    /* { nombre:"Juan",
+    apellido:"Oquendo"
+}
+}
+*/
+// Compare this snippet from clases/clase%15/index.js:
+
+function FactoryMascota(nombre, raza, numeroContacto, dueño) {
+    return {
+        nombre,
+        raza,
+        numeroContacto,
+        IsAtendidos,
+        dueño
+    }
+}
+
+function FactoryDueño(nombre, apellido) {
+    return {
+        nombre,
+        apellido
+    }
+}
+
+let mascota = FactoryMascota("Rocky Ronaldo", 
+"criollo",
+ "300000123",
+ true, 
+ FactoryDueño("Juan", "Oquendo"))
+
+let mascota1 = FactoryMascota("Firulais",
+    "Pitbull",
+    "123456",
+    false,
+    FactoryDueño("Miguel", "Alvarez"))
+
+function Veterinaria() {
+    this.listaAtendidos = []
+    this.listaNoAtendidos = []
+
+}
+
+// Path: clases/clase%2017/index.js
+
+
+
+
