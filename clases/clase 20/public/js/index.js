b5094b237b7a90575535e95fd830f0d300e58755
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-10 19:47:38 -0500

    Clase 20 queryselector y adeventlistener

---
+++ clases/clase 20/public/js/index.js
@@ -0,0 +1,7 @@
+const tarjetas = document.querySelector('.tarjetas');
+const btnClick = document.querySelector('#btnClick');
+const tarjeta = document.querySelector('.tarjeta');
+
+btnClick.addEventListener('click', () => {
+    tarjetas.appendChild(tarjeta.cloneNode(true));
+});
