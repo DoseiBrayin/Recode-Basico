5532010a195419390e337f70417c9bb67d360ebd
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-17 16:48:45 -0500

    RESUELT EJERCICIO 21

---
+++ resuelto/ejercicion21/public/js/index.js
@@ -0,0 +1,32 @@
+const inputNombre = document.querySelector('#inputNombre')
+const inputApellido = document.querySelector('#inputApellido')
+const inputCedula = document.querySelector('#inputCedula')
+const inputFecha = document.querySelector('#inputNacimiento')
+
+const btnGuardar = document.querySelector('#btnAdd')
+
+const usuarios = document.querySelector('.usuarios')
+
+console.log(inputNombre, inputApellido, inputCedula, inputFecha, btnGuardar, usuarios)
+
+btnGuardar.addEventListener('click', () => {
+    const nombre = inputNombre.value
+    const apellido = inputApellido.value
+    const cedula = inputCedula.value
+    const fecha = inputFecha.value
+
+    if (!nombre || !apellido || !cedula || !fecha) {
+        alert('Por favor llene todos los campos')
+        return
+    }
+
+    const usuario = document.createElement('div')
+    usuario.classList.add('usuario')
+    usuario.innerHTML = `
+        <h2>${nombre} ${apellido}</h2>
+        <p>Cédula: ${cedula}</p>
+        <p>Fecha de nacimiento: ${fecha}</p>
+    `
+    usuarios.appendChild(usuario)
+
+})
