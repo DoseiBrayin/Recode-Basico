55a57d5cce9a2f9b4b56e651b4cb1f239faec20b
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-06-25 16:17:01 -0500

    Objetos 2

--- clases/clase 26/public/js/index.js
+++ clases/clase 26/public/js/index.js
@@ -1,30 +1,5 @@
 const formulario = document.querySelector('.aggUsuario');
 
-const Usuarios = () => {
-    return {
-        nombre: '',
-        apellido: '',
-        email: '',
-        password: '',
-        url: '',
-
-        setNombre:(nombre) => {
-            this.nombre = nombre;
-        },
-
-        jsonToHtml: () => { 
-            return `
-            <div class="card" style="width: 18rem;">
-                <img src="${this.url}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title
-                    ">${this.nombre} ${this.apellido}</h5>
-                    <p class="card-text">${this.email}</p>
-            </div> `
-    }
-}
-}
-
 
 const agregaLocalStorage = (usuario) => {
     try {
@@ -42,6 +17,40 @@ const obtenerLocalStorage = () => {
     }
 }
 
+const Usuarios = () => {
+    return {
+        nombre: '',
+        apellido: '',
+        email: '',
+        password: '',
+        url: '',
+
+        setNombre: (nombre) => {
+            this.nombre = nombre;
+        },
+
+        getNombre: () => {
+            return this.nombre;
+        },
+
+        jsonToHtml: () => {
+            return `
+                <div class="card" style="width: 18rem;">
+                    <img src="${this.url}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title
+                        ">${this.nombre} ${this.apellido}</h5>
+                        <p class="card-text">${this.email}</p>
+                </div> `
+        }
+    }
+}
+
+
 formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const usuario = Usuarios();
+    usuario.setNombre("Juan");
+    console.log(usuario.getNombre());
 });
 
