1cc6d931108a67810e976be3c8b63e929c85d942
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-07-02 16:38:00 -0500

    Fetch asincrono

--- clases/clase 27/public/js/index.js
+++ clases/clase 27/public/js/index.js
@@ -2,16 +2,19 @@ const todo = document.querySelector("#todos")
 const btnSearch =  document.querySelector("#btnFetch")
 
 
+async function getTodos() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json()
+    return data
+}
 
-btnSearch.addEventListener('click', () => {
-    const data = fetch('https://jsonplaceholder.typicode.com/')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(error))
-    .finally(() => console.log('fetch finalizado'))
-
-    todo.innerHTML = data
-
+btnSearch.addEventListener('click', async () => {
+    const data = await getTodos() 
+    console.log(data)
+    todo.innerHTML = `<li>${data.title}</li>
+                      <li>${data.completed}</li>
+                      <li>${data.id}</li>
+                      <li>${data.userId}</li>`              
 }
 )
 
